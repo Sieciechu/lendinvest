1a28e76cae99f09898ec83b69b4b052f8361f5b0
Sieciech <wojciech.mocek@o2.pl>
2019-07-05 22:35:47 +0200

    Cash: implemented Stringer interface

--- lendinvest.go
+++ lendinvest.go
@@ -1,11 +1,17 @@
 package lendinvest
 
+import "fmt"
+
 type Lendinvest struct {
 	loans     []loan
 	paychecks []*paycheck
 }
 
 type Cash float64
+
+func (c Cash) String() string {
+	return fmt.Sprintf("%.2f", c)
+}
 
 type Investor interface {
 	lendMoney(money Cash) (Cash, error)
