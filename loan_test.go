84501d4a05b74cf1a43c946ffdf631c088b862d6
Sieciech <wojciech.mocek@o2.pl>
2019-07-05 19:47:13 +0200

    Created unit test plus small refactor

--- loan_test.go
+++ loan_test.go
@@ -9,24 +9,51 @@ import (
 
 func TestCalculateNumberOfPaychecks(t *testing.T) {
 
+	date := calendar.NewDate
+
 	cases := []struct {
 		start          time.Time
 		end            time.Time
 		expectedNumber int
 	}{
-		{calendar.NewDate("2015-01-01"), calendar.NewDate("2015-01-01"), 1},
-		{calendar.NewDate("2015-01-31"), calendar.NewDate("2015-01-31"), 1},
-		{calendar.NewDate("2015-01-16"), calendar.NewDate("2015-02-01"), 2},
-		{calendar.NewDate("2015-01-01"), calendar.NewDate("2015-02-01"), 2},
-		{calendar.NewDate("2015-01-01"), calendar.NewDate("2015-01-31"), 1},
-		{calendar.NewDate("2015-10-02"), calendar.NewDate("2015-11-15"), 2},
-		{calendar.NewDate("2015-10-02"), calendar.NewDate("2016-11-15"), 14},
+		{date("2015-01-01"), date("2015-01-01"), 1},
+		{date("2015-01-31"), date("2015-01-31"), 1},
+		{date("2015-01-16"), date("2015-02-01"), 2},
+		{date("2015-01-01"), date("2015-02-01"), 2},
+		{date("2015-01-01"), date("2015-01-31"), 1},
+		{date("2015-10-02"), date("2015-11-15"), 2},
+		{date("2015-10-02"), date("2016-11-15"), 14},
 	}
 	for _, c := range cases {
 		got := calculateNumberOfPaychecks(c.start, c.end)
 		if got != c.expectedNumber {
 			t.Errorf("calculateNumberOfPaychecks(%q, %q), got %d , want %d",
 				c.start, c.end, got, c.expectedNumber)
+		}
+	}
+}
+
+func TestGetNextPaymentDate(t *testing.T) {
+
+	date := calendar.NewDate
+
+	cases := []struct {
+		start    time.Time
+		end      time.Time
+		expected time.Time
+	}{
+		{date("2018-07-01"), date("2018-07-01"), date("2018-07-01")},
+		{date("2018-07-04"), date("2018-07-15"), date("2018-07-15")},
+		{date("2018-07-04"), date("2018-08-15"), date("2018-08-01")},
+		{date("2018-07-31"), date("2018-08-01"), date("2018-08-01")},
+		{date("2018-07-07"), date("2018-12-01"), date("2018-08-01")},
+	}
+
+	for _, c := range cases {
+		got := getNextPaymentDate(c.start, c.end)
+
+		if !c.expected.Equal(got) {
+			t.Errorf("Expected %q, but got %q", c.expected, got)
 		}
 	}
 }
