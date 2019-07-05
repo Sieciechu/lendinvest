26306511748da2b1da865a13fbb19ef052f04415
Sieciech <wojciech.mocek@o2.pl>
2019-07-05 21:19:04 +0200

    Added documentation and small refactor

--- investment.go
+++ investment.go
@@ -25,36 +25,34 @@ type paycheck struct {
 	title         string
 }
 
+// Factory function to create new investment and calculates future paychecks
+// so we know in advance what money we will have to transfer
+// when time of paycheck will occur
 func newInvestment(i *Investor, m Cash, monthlyInterestPercetage uint,
 	start time.Time, end time.Time) investment {
-
-	n := calculateNumberOfPaychecks(start, end)
 
 	investment := investment{investor: i,
 		investedMoney: m,
 		startDate:     start,
 		endDate:       end,
-		paychecks:     make([]paycheck, n)}
+		paychecks:     nil}
 
 	investment.calculatePaychecks()
 
 	return investment
 }
 
-func calculateNumberOfPaychecks(start, end time.Time) (n int) {
-	for end.After(start) || end.Equal(start) {
-		n++
-		start = start.AddDate(0, 1, (-start.Day() + 1))
-	}
-	return
-}
-
+// Support method for factory newInvestment()
+// Creates paychecks according to investment
 func (investment *investment) calculatePaychecks() {
 
+	investment.paychecks = nil
+	
+	count := calculateNumberOfPaychecks(investment.startDate, investment.endDate)
+	
 	periodStart := investment.startDate
 	end := investment.endDate
-
-	for k, size := 0, len(investment.paychecks); k < size; k++ {
+	for k := 0; k < count; k++ {
 
 		nextPaymentDate := getNextPaymentDate(periodStart, end)
 
@@ -63,7 +61,7 @@ func (investment *investment) calculatePaychecks() {
 			periodStart: periodStart, periodEnd: nextPaymentDate, dateOfPayment: nextPaymentDate,
 			moneyToPay: moneyToPay, paid: false, title: "income from investment"}
 
-		investment.paychecks[k] = p
+		investment.paychecks = append(investment.paychecks, p)
 
 		periodStart = nextPaymentDate.AddDate(0, 0, 1)
 	}
@@ -75,6 +73,17 @@ func (investment *investment) calculatePaychecks() {
 	investment.paychecks = append(investment.paychecks, returnOfInvestedMoney)
 }
 
+// Support method for calculatePaychecks()
+// Calculates number of paychecks
+func calculateNumberOfPaychecks(start, end time.Time) (n int) {
+	for end.After(start) || end.Equal(start) {
+		n++
+		start = start.AddDate(0, 1, (-start.Day() + 1))
+	}
+	return
+}
+
+// Support method for calculatePaychecks()
 func getNextPaymentDate(start, end time.Time) time.Time {
 
 	if end.Year() == start.Year() && end.Month() == start.Month() {
@@ -84,6 +93,8 @@ func getNextPaymentDate(start, end time.Time) time.Time {
 	return start.AddDate(0, 1, (-start.Day() + 1)) // set date to 1st day of next month
 }
 
+// It calculates how much money should be paid for each payment period (each paycheck)
+// Method used in loop as support method for calculatePaychecks()
 func (investment *investment) calculateMoneyToPayForPeriod(start, end time.Time) Cash {
 
 	maxDaysInCurrentPaymentMonth := float64(calendar.GetLastDayOfMonth(start))
