a9da5ba9d3bf437d363aef81c039158fffd19299
Sieciech <wojciech.mocek@o2.pl>
2019-07-06 22:44:31 +0200

    Added documentation

--- user.go
+++ user.go
@@ -2,10 +2,12 @@ package lendinvest
 
 import "errors"
 
+// User - a type who will be able to invest
 type User struct {
 	money Cash
 }
 
+// Implementation of the Investor interface
 func (u *User) LendMoney(money Cash) (Cash, error) {
 	if money > u.money {
 		return 0, errors.New("User has not enough money to lend")
@@ -14,6 +16,8 @@ func (u *User) LendMoney(money Cash) (Cash, error) {
 
 	return money, nil
 }
+
+// Implementation of the Investor interface
 func (u *User) TakeMoney(money Cash) {
 	u.money += money
 }
